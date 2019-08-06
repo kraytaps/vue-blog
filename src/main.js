606e5be9044c44d27fd7aa6cead15c654e6026ef
Chris <chris.791.ix@gmail.com>
2019-08-05 21:21:34 -0700

    added router, locally defined filters, directives, mixins

--- src/main.js
+++ src/main.js
@@ -1,32 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
-// Custom Directives
-Vue.directive('rainbow', {
-  bind(el, binding, vnode){
-    el.style.color = '#' + Math.random().toString().slice(2,8)
-  }
+// VueRouter
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history',
 });
 
-Vue.directive('theme', {
-  bind(el, binding, vnode){
-    if (binding.value == 'wide'){
-      el.style.maxWidth = '1200px';
-    } else if (binding.value == 'narrow'){
-      el.style.maxWidth = '560px';
-    }
-    if (binding.arg == 'column'){
-      el.style.background = '#ddd';
-      el.style.padding = '20px';
-    }
-  }
-})
+// Custom Directives
+// Vue.directive('rainbow', {
+//   bind(el, binding, vnode){
+//     el.style.color = '#' + Math.random().toString().slice(2,8)
+//   }
+// });
+
+// Vue.directive('theme', {
+//   bind(el, binding, vnode){
+//     if (binding.value == 'wide'){
+//       el.style.maxWidth = '1200px';
+//     } else if (binding.value == 'narrow'){
+//       el.style.maxWidth = '560px';
+//     }
+//     if (binding.arg == 'column'){
+//       el.style.background = '#ddd';
+//       el.style.padding = '20px';
+//     }
+//   }
+// });
+
+// Custom Filters
+// Vue.filter('snippet', (value) => {
+//   return value.slice(0, 100) + '...';
+// })
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: router,
 }).$mount('#app')
